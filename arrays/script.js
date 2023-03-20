45232d023382cf734b636520ae07ced514e64d5f
Luis Felipe Pizzi Lopes <luisfelipe1203lf@gmail.com>
2023-03-20 18:08:33 -0300

    Arrays

---
+++ arrays/script.js
@@ -0,0 +1,9 @@
+var objeto = {
+    nome: "luis", 
+    idade: 19, 
+    genero: "masculino", 
+    curso: "análise e desenvolvimento de sistemas", 
+    estudando: true
+}
+
+console.log(objeto.nome); 
