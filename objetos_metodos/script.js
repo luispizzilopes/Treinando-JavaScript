b6ae9ad5963301c05b3b1279fdca5a2ccfe39ad3
Luis Felipe Pizzi Lopes <luisfelipe1203lf@gmail.com>
2023-03-21 14:45:02 -0300

    Objetos com métodos

---
+++ objetos_metodos/script.js
@@ -0,0 +1,10 @@
+let livro = {
+    nomeLivro: 'livro', 
+    qntPaginas: 100, 
+    autorLivro: 'teste', 
+    imprimirLivro: function(){
+        console.log(livro); 
+    }
+}
+
+livro.imprimirLivro();
